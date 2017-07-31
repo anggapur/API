7fe1d6656a4bd99de800894640d280f82bfe7fb9
anggapur <arimbawaangga@gmail.com>
2017-07-31 07:52:33 -0700

    tambah key

---
+++ app/Http/Controllers/sliderCtrl.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\slider;
+use JP;
+class sliderCtrl extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        $query = slider::all();
+        if($query)
+        {
+            $data['message'] = "success";
+            $data['results'] = $query;
+        }
+        else
+        {
+            $data['message'] = "failed";
+        }
+        return JP::prints($data);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,9 @@ Route::get('/', function () {
 Route::get('notValid',function(){
 	return view('notValid');
 	});
-Route::middleware([])->group(function(){
+Route::middleware(['cekApiKey'])->group(function(){
 	Route::get('products/search/{q}','productCtrl@search');
 	Route::resource('products','productCtrl');
+	Route::resource('sliders','sliderCtrl');
 	Route::resource('categories','categoryCtrl');
 });
